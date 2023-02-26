fb28ad13d980cba2ed325753b2ad6ffbac5d5ecb
Mamitiana <mamitiana@fulldigits.com>
2023-02-26 19:39:43 +0300

    Just reminder, here is how to pass var into the return of the JSX, Going to do how to change that name001 when we clicked on the button

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,8 +1,10 @@
 const Home = () => {
+  let name = "Mario";
   const handleClick = () => {};
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
