03c8643af62f9c6f2951c4126dff35326d42f51a
Mamitiana <mamitiana@fulldigits.com>
2023-02-26 19:37:45 +0300

    Here is what he has in the beginning of this video

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,9 @@
 const Home = () => {
-  const handleClick = (e) => {
-    console.log("hello ninjas", e);
-  };
-  const handleClickAgain = (name, e) => {
-    console.log("hello " + name, e.target);
-  };
+  const handleClick = () => {};
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("mario", e)}>
-        Click me again
-      </button>
     </div>
   );
 };
