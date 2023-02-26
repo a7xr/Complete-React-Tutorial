0b5b0a38e181a03710cfb2811fe15b6b5255733c
Mamitiana <mamitiana@fulldigits.com>
2023-02-26 21:30:25 +0300

    Once again, u should read this commit often, Better to set in another js_file while listing the blog

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,3 +1,18 @@
+const BlogList = () => {
+  return (
+    <div className="blog-list">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
+
 // const BlogList = ({ blogs, title}) => {
 //   // const blogs = props.blogs;
 //   // const title = props.title;
