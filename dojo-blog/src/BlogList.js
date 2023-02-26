c87535d745949bde7e37b42bd5605a013c6cf3f4
Mamitiana <mamitiana@fulldigits.com>
2023-02-27 02:24:11 +0300

    Here is how to manage the var that is received from parent_component, it is called props

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,7 +1,8 @@
-const BlogList = () => {
+const BlogList = (props) => {
+  const blogs = props.blogs;
   return (
     <div className="blog-list">
-      {blogs.map((blog) => (
+      {blogs.map((blog) => ( 
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
