b4dbbf356363c9856ec7b19c007b39463bd52c24
Mamitiana <mamitiana@fulldigits.com>
2023-02-26 11:06:03 +0300

    Playing around with jsx

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,10 +2,18 @@ import "./App.css";
 
 function App() {
   const title = "Welcome to the new blog";
+  const likes = 50;
+  const link = "http://www.google.com/"
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
+        <p>Liked {likes}</p>
+
+        <p>{10}</p>
+        <p>{"This is a String"}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
