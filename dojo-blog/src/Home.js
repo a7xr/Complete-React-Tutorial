3a1f52338bed59d07f20dcf1b4e0f372c3d56ab6
Mamitiana <mamitiana@fulldigits.com>
2023-02-27 02:51:05 +0300

    Set Home.js like in the beginning of the vid

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -8,14 +8,10 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 }
