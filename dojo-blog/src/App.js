5200aafb68064f303162cab5899083b96187ef90
Mamitiana <mamitiana@fulldigits.com>
2023-02-26 10:51:59 +0300

    Commented everything and going to grab everything from lesson-2_001

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="content">
-        <h1>App Component</h1>
-      </div>
-    </div>
-  );
-}
+// import './App.css';
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="content">
+//         <h1>App Component</h1>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
