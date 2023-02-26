89207daa23d457e8fe583b58048db2d4bd51085d
Mamitiana <mamitiana@fulldigits.com>
2023-02-27 02:53:02 +0300

    We want to create a button able to delete a blog, that function Must be created on the Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,17 +3,23 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
+  const handleDelete = (id) => {};
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
-}
- 
+};
+
 export default Home;
