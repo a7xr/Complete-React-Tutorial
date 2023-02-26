5f92a56085edec73f4a2aee9291fbe3f1ed292c9
Mamitiana <mamitiana@fulldigits.com>
2023-02-27 02:29:04 +0300

    Fix, Here is how to pass a var from parent_component to child_component

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {/* <BlogList blogs={blogs} title="All Blogs" /> */}
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 };
