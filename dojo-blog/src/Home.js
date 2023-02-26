f1eba790b8821323e943f6abe1e73c92d9862699
Mamitiana <mamitiana@fulldigits.com>
2023-02-26 20:38:27 +0300

    Added another usage of useState, so there are 2 collapsable in the tab_component__hooks

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,13 +3,16 @@ import { useState } from "react";
 const Home = () => {
   //   let name = "Mario";
   const [name, setName] = useState("Mario");
+  const [age, setAge] = useState(25);
   const handleClick = () => {
     setName("Luigi");
+    setAge(30);
   };
   return (
     <div className="home">
       <h2>Homepage</h2>
       <p>{name}</p>
+      <p>{age}</p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
