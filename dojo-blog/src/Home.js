8be001ffc9920d8f3f4e411b6f5fbe0be4f449ba
Mamitiana <mamitiana@fulldigits.com>
2023-03-01 00:02:10 +0300

    Like in the beginning of the video for Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,29 +3,30 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-  const [name, setName] = useState('mario');
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
-  }
+  };
 
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-  }, [name])
+    console.log("use effect ran");
+  }, []);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
     </div>
   );
-}
- 
+};
+
 export default Home;
