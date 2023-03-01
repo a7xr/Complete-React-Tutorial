be69f2bce76d876068331e098b24395239900ac7
Mamitiana <mamitiana@fulldigits.com>
2023-03-01 08:00:57 +0300

    Going to do Home.js like in the beginning of the video

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,23 +2,15 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-      })
-  }, [])
+    console.log("use effect ran");
+  }, []);
 
-  return (
-    <div className="home">
-      {blogs && <BlogList blogs={blogs} />}
-    </div>
-  );
-}
- 
+  return <div className="home">
+      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}></BlogList>
+  </div>;
+};
+
 export default Home;
