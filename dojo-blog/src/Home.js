a1fbcd05c21ad232c704963d240a8ecfdc199a74
Mamitiana <mamitiana@fulldigits.com>
2023-03-01 08:04:49 +0300

    We set the initialization of data_blogs to null, and going to fetch data in useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,16 +2,7 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
